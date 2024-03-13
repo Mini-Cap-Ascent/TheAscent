ae0b8ba14460d9ba7ec60c167ffeb3f45589dc40
dhruvsaikia <85579996+dhruvsaikia@users.noreply.github.com>
2024-03-13 15:43:42 -0400

    Add files via upload

--- Assets/Game/Scripts/PlayerMovement.cs
+++ Assets/Game/Scripts/PlayerMovement.cs
@@ -1,14 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Splines;
+using Fusion;
 
-public class PlayerMovement : MonoBehaviour
+
+public class PlayerMovement : NetworkBehaviour
 {
     public float moveSpeed = 10.0f;
     private PlayerCont controls;
     private Vector2 move;
     private Rigidbody rb;
 
-/*    public GameObject cameraTarget;*/ // Assign this in the inspector
-    public float rotationSpeed = 5f; // Adjust as needed
+    private NetworkCharacterController _cc;
+
+    public override void FixedUpdateNetwork()
+    {
+        if (GetInput(out NetworkInputData data))
+        {
+            data.direction.Normalize();
+            _cc.Move(5 * data.direction * Runner.DeltaTime);
+        }
+    }
 
     private void Awake()
     {
@@ -17,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
 
         controls.PlayerControlz.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
         controls.PlayerControlz.Move.canceled += ctx => move = Vector2.zero;
+        _cc = GetComponent<NetworkCharacterController>();
     }
 
     private void OnEnable()
@@ -28,23 +42,5 @@ public class PlayerMovement : MonoBehaviour
     {
         controls.Disable();
     }
-
-    private void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(move.x, 0, move.y) * moveSpeed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + movement);
-
-        //// Update camera target's position to player's position
-        //if (cameraTarget != null)
-        //{
-        //    cameraTarget.transform.position = rb.position;
-        //    if (move != Vector2.zero)
-        //    {
-        //        // Calculate the rotation towards the movement direction
-        //        Quaternion targetRotation = Quaternion.LookRotation(new Vector3(move.x, 0, move.y));
-        //        // Smoothly rotate the camera target towards the calculated rotation
-        //        cameraTarget.transform.rotation = Quaternion.Slerp(cameraTarget.transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
-        //    }
-        //}
-    }
 }
+
