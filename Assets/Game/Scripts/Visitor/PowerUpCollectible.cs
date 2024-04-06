79ea25c427bd9e3ad1efe1404ae5c75519272dd7
bennieb1 <brandon961@live.ca>
2024-04-06 15:47:52 -0400

    Revert "Merge branch 'main' into MakingThings"
    This reverts commit 4d91edfc164a042506212f042732b3cc773cc9f0, reversing
    changes made to 6041114abb2c3f1de6e0bf650229358bb015c393.

---
+++ Assets/Game/Scripts/Visitor/PowerUpCollectible.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PowerUpCollectible : MonoBehaviour, ICollectible
+{
+    // Define power-up specific fields
+
+    public void Accept(IPlayerVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+
+
+    //add sword powerup using visitor interface
+}
