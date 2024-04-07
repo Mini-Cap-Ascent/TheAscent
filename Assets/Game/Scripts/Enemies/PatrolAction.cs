63ea1d59e159d829ce589068c397c5316dabd292
bennieb1 <brandon961@live.ca>
2024-04-07 15:28:04 -0400

    123

---
+++ Assets/Game/Scripts/Enemies/PatrolAction.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+[TaskName("PatrolAction")]
+[TaskCategory("Generic")]
+[TaskDescription("Patrol the area using the NavMeshAgent")]
+
+public class PatrolAction : MoveToGoal
+{
+
+    public SharedGameObjectList waypoints;
+    public bool loop = true;
+    private int currentWaypointIndex = 0;
+
+    public override void OnStart()
+    {
+        base.OnStart();
+        if (currentWaypointIndex < waypoints.Value.Count)
+        {
+
+            agent.isStopped = false;
+            agent.SetDestination(waypoints.Value[currentWaypointIndex].transform.position);
+
+        }
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+     
+        TaskStatus baseStatus = base.OnUpdate();
+        if (baseStatus != TaskStatus.Running && currentWaypointIndex != waypoints.Value.Count)
+        {
+
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Value.Count && loop == true) {
+            
+                currentWaypointIndex = 0;
+            
+            }
+
+            if (currentWaypointIndex < waypoints.Value.Count)
+            {
+                agent.SetDestination(waypoints.Value[currentWaypointIndex].transform.position);
+            
+                return TaskStatus.Running;
+            }
+
+        }
+        return baseStatus;
+    }
+
+}
